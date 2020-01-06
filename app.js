bc9eef23fd6f18f324411d666417773b95e6e427
Thom Wise <thom.gkw.wp@gmail.com>
2020-01-05 17:11:05 -0800

    Added attendee list area with delete button control

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+// DASHBOARD CONTROLLER
+var dashboardController = (function() {
 
+})();
+
+// UI CONTROLLER
+var UIController = (function() {
+
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function() {
+
+    
+})(dashboardController, UIController);
+
+controller.init();
